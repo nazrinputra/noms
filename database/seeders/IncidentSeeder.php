1f33c225b1450a6885434b0a72dbf134a226c834
Nazrin Putra <73219506+nazrinputra@users.noreply.github.com>
2022-03-08 22:12:01 +0800

    Modify seeders

--- database/seeders/IncidentSeeder.php
+++ database/seeders/IncidentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Incident;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,6 @@ class IncidentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Incident::factory(10)->create();
     }
 }
