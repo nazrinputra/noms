df2d7aec9bef4482a2e51cc6802968ac7a123d53
Nazrin Putra <73219506+nazrinputra@users.noreply.github.com>
2022-03-08 17:32:09 +0800

    Modify welcome layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,31 @@
 </head>
 
 <body class="antialiased">
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="relative bg-white">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6">
+            <div
+                class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+                <div class="flex justify-start lg:w-0 lg:flex-1">
+                    <a href="#">
+                        <span class="sr-only">Workflow</span>
+                        <img class="h-8 w-auto sm:h-10"
+                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
+                    </a>
+                </div>
+
+                <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                    <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                        Login
+                    </a>
+                    <a href="#"
+                        class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                        Register </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
 
@@ -122,12 +147,7 @@
                 <div class="inline-flex rounded-md shadow">
                     <a href="{{ route('login') }}"
                         class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
-                        Login </a>
-                </div>
-                <div class="ml-3 inline-flex rounded-md shadow">
-                    <a href="{{ route('register') }}"
-                        class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">
-                        Register </a>
+                        Get Started </a>
                 </div>
             </div>
         </div>
