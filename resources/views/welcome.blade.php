752b67d783833ea83642708308524095e9237cc5
Nazrin Putra <73219506+nazrinputra@users.noreply.github.com>
2022-03-08 17:41:05 +0800

    Fix link for login and register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,10 +25,11 @@
                 </div>
 
                 <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                    <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                    <a href="{{ route('login') }}"
+                        class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                         Login
                     </a>
-                    <a href="#"
+                    <a href="{{ route('register') }}"
                         class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
                         Register </a>
                 </div>
