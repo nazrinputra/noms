ef6d74bf82931035a38360c586132c681d2645fc
Nazrin Putra <73219506+nazrinputra@users.noreply.github.com>
2022-03-09 08:02:13 +0800

    force https

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+URL::forceScheme('https');
 
 Route::get('/', function () {
     return view('welcome');
